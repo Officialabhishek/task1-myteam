751af71e40149fad5013bbe8a09ef6b52cdb6bfe
Abhishek Nagar <abhisheknagar0110@gmail.com>
2023-05-04 17:55:42 +0530

    added js file

---
+++ card.js
@@ -0,0 +1,60 @@
+var btn1=document.getElementById("card1");
+        var close1=document.getElementById("card1_hidden");
+        function showjs1(){
+            card1.style.display="none";
+            card1_hidden.style.display="block";
+        }
+        function hidejs1(){
+            card1.style.display="block";
+            card1_hidden.style.display="none";
+        }
+        var btn2=document.getElementById("card2");
+        var close2=document.getElementById("card2_hidden");
+        function showjs2(){
+            card2.style.display="none";
+            card2_hidden.style.display="block";
+        }
+        function hidejs2(){
+            card2.style.display="block";
+            card2_hidden.style.display="none";
+        }
+        var btn3=document.getElementById("card3");
+        var close3=document.getElementById("card3_hidden");
+        function showjs3(){
+            card3.style.display="none";
+            card3_hidden.style.display="block";
+        }
+        function hidejs3(){
+            card3.style.display="block";
+            card3_hidden.style.display="none";
+        }
+        var btn4=document.getElementById("card4");
+        var close4=document.getElementById("card4_hidden");
+        function showjs4(){
+            card4.style.display="none";
+            card4_hidden.style.display="block";
+        }
+        function hidejs4(){
+            card4.style.display="block";
+            card4_hidden.style.display="none";
+        }
+        var btn5=document.getElementById("card5");
+        var close5=document.getElementById("card5_hidden");
+        function showjs5(){
+            card5.style.display="none";
+            card5_hidden.style.display="block";
+        }
+        function hidejs5(){
+            card5.style.display="block";
+            card5_hidden.style.display="none";
+        }
+        var btn6=document.getElementById("card6");
+        var close6=document.getElementById("card6_hidden");
+        function showjs6(){
+            card6.style.display="none";
+            card6_hidden.style.display="block";
+        }
+        function hidejs6(){
+            card6.style.display="block";
+            card6_hidden.style.display="none";
+        }
